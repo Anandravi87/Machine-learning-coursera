7ca48b7dff33793468894ef9ba70ca0b15125295
Anand Kumar Ravi <anandkumarravi2016@gmail.com>
2020-06-10 12:14:51 +0800

    Uploaded the week3 files from my mac

--- plotData.m
+++ plotData.m
@@ -1,25 +1,32 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
+%First, find the indices of positive and negative values of y
+pos = find(y==1);
+neg = find(y==0);
 
-plot(x, y, 'rx', 'MarkerSize', 5); % Plot the dataylabel('Profit in $10,000s'); % Set the y􀀀axis labelxlabel('Population of City in 10,000s'); % Set the x􀀀axis label
+%plot the positives 
+plot(X(pos,1),X(pos,2), 'k+', 'markersize', 7,'markerfacecolor', 'b');
+plot(X(neg,1),X(neg,2), 'ko', 'markersize', 7,'markerfacecolor', 'y');
 
+xlabel('Exam 1 score');
+ylabel('Exam 2 score');
 
-% ============================================================
+legend('Admitted', 'Not admitted');
+
+% =========================================================================
+
+hold off;
 
 end
